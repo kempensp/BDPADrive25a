631400a22d618866f126d67ca353335c488bf1e1
RamenXk <158761011+RamenXk@users.noreply.github.com>
2025-06-28 17:32:16 +0000

    Added password attempts and forgot password link and page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
+var passwordRouter = require('./routes/password');
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use(function (req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use('/password', passwordRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
